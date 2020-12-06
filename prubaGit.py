d5556ee64a2f152e538e98b50afdefadcac4ba5e
AbielBorja <abielboga@gmail.com>
2020-12-06 16:02:53 -0600

    Yayo master

--- prubaGit.py
+++ prubaGit.py
@@ -1,3 +1,5 @@
 #git add .
 #git commit -m "mensaje del commit"
-#git push origin master || la rama que deseas.
+#git push origin master || la rama que deseas.  
+
+print("Yayo es un gran maestro :D")
